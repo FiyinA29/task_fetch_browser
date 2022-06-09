e600a9a4d186e802b48a240e6dfb3deebf024bfc
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-06-09 16:41:04 +0100

    displayAllCountries function added

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 console.log("HI!")
 
-const getCountryByName = (countryName) => {
+// //FOR A CHOSEN COUNTRY
+// const getCountryByName = (countryName) => {
+//     const section = document.getElementById("section");//To access the section tag created in the html
+//     const text = document.createElement("p"); //creates a paragraph tag for the text
+//     section.append(text);
+//     fetch(`https://restcountries.com/v2/name/${countryName}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             text.innerHTML = `Country: ${data[0].name} <br> Population: ${data[0].population}`;
+//         })
 
-    fetch(`https://restcountries.com/v2/name/${countryName}`)
+// }
+// getCountryByName("nigeria");
+
+
+//FOR ALL COUNTRIES
+const getAllCountries = () => {
+    fetch("https://restcountries.com/v2/all")
         .then(response => response.json())
-        .then((data) => {
-            text.innerHTML = `Country: ${data[0].name} <br> Population: ${data[0].population}`;
+        .then(data => {
+            for (let i = 0; i < 250; i++) {
+                const section = document.getElementById("section");
+                const text = document.createElement("p");
+                section.append(text);
+                text.innerHTML = `Country: ${data[i].name} <br> Population: ${data[i].population}`;
+            }
         })
-
 }
-
-const section = document.getElementById("section");//To access the section tag created in the html
-const text = document.createElement("p"); //creates a paragraph tag for the text
-section.append(text);
-getCountryByName("nigeria");
-
-
+getAllCountries();
